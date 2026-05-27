2ff7bc06b7879f17def316ec8ac270fe536e1a6b
AnTiCaff3 <findukmult@gmail.com>
2026-05-27 12:36:28 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+console.log("Сайт готов к работе в Переславле!");
